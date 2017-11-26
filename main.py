dae34a68e119c164d22a799f8f41fc7e7d817808
Krzysztof Czuryło <krzysztof.czurylo@gmail.com>
2017-11-26 15:52:33 +0100

    Implemented image hashing based on average image color

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import imageio
 from pprint import pprint
+from PIL import Image
 
 def averageVideo(videoUrl):
     reader = imageio.get_reader(videoUrl)
@@ -13,8 +14,20 @@ def averageVideo(videoUrl):
         average += im
         frames += 1
     average = average / frames
+    return average
+
+def hashImage(image):
+    im = Image.fromarray(np.uint8(image))
+    transformedImage = im.resize((8,9), Image.ANTIALIAS)
+    transformedImage = transformedImage.convert("L")
+    listOfColors = list(transformedImage.getdata())
+    averageColor = sum(listOfColors) / len(listOfColors)
+    bits = "".join(map(lambda pixelColor: '1' if pixelColor > averageColor else '0', listOfColors))
+    hash = int(bits,2).__format__('016x').upper()
+    pprint(hash)
 
 if __name__ == '__main__':
     start_time = time.time()
-    averageVideo('D:/BigDataChallenge/videos/00SXM76KD1X2.mp4')
+    average = averageVideo('D:/BigDataChallenge/videos/00SXM76KD1X2.mp4')
+    hashImage(average)
     print("Execution time: %s seconds." % (time.time() - start_time))
